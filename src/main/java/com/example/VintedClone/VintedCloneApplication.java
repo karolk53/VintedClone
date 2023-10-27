e9e1f27a1d98eae27d2633d84f26ce63605ade5b
Karol <karolk1232.kk@gmail.com>
2023-10-27 10:25:11 +0200

    Initial commit

---
+++ src/main/java/com/example/VintedClone/VintedCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.VintedClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintedCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintedCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VintedClone/VintedCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VintedClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintedCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
